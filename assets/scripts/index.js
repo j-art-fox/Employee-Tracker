4535dce6f8d8c90ab22645056d44e230d95b5f12
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2022-09-02 18:10:10 +0900

    created main menu

---
+++ assets/scripts/index.js
@@ -0,0 +1,76 @@
+import inquirer from 'inquirer';
+// const fs = require('fs');
+
+
+function runMainMenu() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Main Menu',
+                name: "mainMenu",
+                choices: ["view all departments", "view all roles", " view all employees", "add a department", "add a role", "add an employee", "update an employee role"]
+            }
+        ])
+
+        .then((data) => {
+            switch (data) {
+                case 'view all departments':
+                    viewDepartments();
+                    break;
+                case 'view all roles':
+                    viewRoles();
+                    break;
+                case 'view all employees':
+                    viewEmployees();
+                    break;
+                case 'add a department':
+                    addDepartment();
+                    break;
+                case 'add a role':
+                    addRole();
+                    break;
+                case 'add an employee':
+                    addEmployee();
+                    break;
+                case 'update an employee role':
+                    updateEmpRole();
+                    break;
+
+            }
+        });
+}
+
+function viewDepartments() {
+    console.log("viewDepartments");
+}
+
+function viewRoles() {
+    console.log("viewDepartments");
+}
+
+function viewEmployees() {
+    console.log("viewEmployees");
+}
+
+function addDepartment() {
+    console.log("addDepartment");
+}
+
+function addRole() {
+    console.log("addRole");
+}
+
+function addEmployee() {
+    console.log("addEmployee");
+}
+
+function updateEmpRole(){
+    console.log("updateEmpRole")
+}
+
+function init() {
+    runMainMenu();
+};
+
+init()
